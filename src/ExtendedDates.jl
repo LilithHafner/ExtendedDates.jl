f8a61f9badfd9e7da4651d4193b410d734c133f6
Lilith Hafner <Lilith.Hafner@gmail.com>
2022-06-17 08:43:11 -0400

    Initial content

--- src/ExtendedDates.jl
+++ src/ExtendedDates.jl
@@ -1,5 +1,74 @@
 module ExtendedDates
 
-# Write your package code here.
+using Reexport
+@reexport using Dates
+
+import Base: +, -, isfinite, isless, :, print, show
+import Dates: Date, year
+
+export period, frequency, subperiod, Undated
+
+# Not `using Dates: UTInstant` to avoid type piracy
+struct UTInstant{P<:Period} <: Dates.Instant
+    periods::P
+end
+const YearPeriods = Union{Month, Quarter, Year}
+const DayPeriods = Union{Week, Day}
+
+# Defining the epochs
+epoch(::Type{<:Period}) = Date(0)
+epoch(::Type{Week}) = Date(0, 1, 3) # Monday
+
+# Non overflow checking constructors
+period(P::Type{<:Period}, year::Integer) = period(P, year, 1)
+period(P::Type{<:YearPeriods}, year::Integer, subperiod::Integer) = 
+    UTInstant(P(Year(1) ÷ P(1) * year + subperiod - 1))
+period(P::Type{<:DayPeriods}, year::Integer, subperiod::Integer) =
+    UTInstant(P((Date(year) - epoch(P)) ÷ P(1) + subperiod))
+
+# Conversion to Date to calculate year and subperiod
+Date(p::UTInstant) = Date(0) + p.periods
+Date(p::UTInstant{Week}) = Date(0, 1, 3) + p.periods
+
+# Fallback accessors for frequency, year, subperiod
+frequency(::UTInstant{P}) where P <: Period = oneunit(P)
+year(p::UTInstant) = year(Date(p))
+subperiod(p::UTInstant) = cld((Date(p) - floor(Date(p), Year)), frequency(p))
+
+# Avoid conversion to Date for Year based periods
+year(p::UTInstant{<:YearPeriods}) = p.periods ÷ Year(1)
+subperiod(p::UTInstant{<:YearPeriods}) = (p.periods % Year(1)) ÷ frequency(p) + 1
+
+# Arithmetic and comparison (for ranges)
+isless(a::UTInstant, b::UTInstant) = isless(a.periods, b.periods)
+(+)(a::UTInstant, b::Period) = UTInstant(a.periods + b)
+(-)(a::UTInstant, b::Period) = UTInstant(a.periods - b)
+isfinite(a::UTInstant) = true # Due to julia bug, fix in #45646
+(:)(start::UTInstant{P}, stop::UTInstant{P}) where {P} = start:P(1):stop
+
+# print (for conversion to human readable/standard form string)
+prefix(::Type{Quarter}) = 'Q'
+prefix(::Type{Week}) = 'W'
+prefix(::Type{Day}) = 'D'
+print(io::IO, p::UTInstant{P}) where P = print(io, year(p), '-', prefix(P), subperiod(p))
+print(io::IO, p::UTInstant{Year}) = print(io, year(p))
+print(io::IO, p::UTInstant{Month}) = print(io, year(p), '-', lpad(subperiod(p), 2, '0'))
+
+# show
+function show(io::IO, p::UTInstant) 
+    print(io, UTInstant, '(')
+    show(io, p.periods)
+    print(io, ')')
+end
+
+# Undated
+struct Undated
+    value::Int
+end
+isless(a::Undated, b::Undated) = isless(a.value, b.value)
+(+)(a::Undated, b::Integer) = Undated(a.value + b)
+(-)(a::Undated, b::Integer) = Undated(a.value - b)
+(-)(a::Undated, b::Undated) = a.value - b.value
+isfinite(a::Undated) = true # Due to julia bug, fix in #45646
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,82 @@
 using ExtendedDates
-using Test
+using Test, BenchmarkTools
 
-@testset "ExtendedDates.jl" begin
-    # Write your tests here.
+### Represent periods (time intervals) of different frequencies: year 2022, 2nd quarter of 200, …
+### Constructors with (year, subperiod, frequency)
+year_2022 = period(Year, 2022)
+second_quarter_of_200 = period(Quarter, 200, 2)
+third_week_of_1935 = period(Week, 1935, 3)
+hundredth_day_of_year_54620 = period(Day, 54620, 100)
+undated_12 = Undated(12)
+
+### Periods can be identified by (year, subperiod, frequency)
+
+@testset "year" begin
+    @test year(year_2022) == 2022
+    @test year(second_quarter_of_200) == 200
+    @test year(third_week_of_1935) == 1935
+    @test year(hundredth_day_of_year_54620) == 54620
+    @test_throws MethodError year(undated_12)
+end
+
+@testset "subperiod" begin
+    @test subperiod(year_2022) == 1
+    @test subperiod(second_quarter_of_200) == 2
+    @test subperiod(third_week_of_1935) == 3
+    @test subperiod(hundredth_day_of_year_54620) == 100
+    @test_throws MethodError subperiod(undated_12)
+end
+
+@testset "frequency" begin
+    @test frequency(year_2022) == Year(1)
+    @test frequency(second_quarter_of_200) == Quarter(1)
+    @test frequency(third_week_of_1935) == Week(1)
+    @test frequency(hundredth_day_of_year_54620) == Day(1)
+    @test_throws MethodError frequency(undated_12)
+end
+
+# Range operations on dates
+@testset "ranges" begin
+    twenty_fourth_week_of_1932 = 
+
+    weeks = period(Week, 1932, 24):third_week_of_1935
+    @test period(Week, 1932, 45) ∈ weeks
+    @test period(Week, 1931, 45) ∉ weeks
+
+    @test Undated(17) ∈ Undated(17):Undated(17)
+    @test Undated(4) ∉ Undated(-4):Undated(2)
+end
+
+# Print/string/display/show
+@testset "string" begin
+    @test string(year_2022) == "2022"
+    @test string(second_quarter_of_200) == "200-Q2"
+    @test string(third_week_of_1935) == "1935-W3"
+    @test string(hundredth_day_of_year_54620) == "54620-D100"
+    @test string(undated_12) == "Undated(12)"
+end
+@testset "repr" begin
+    @test endswith(repr(year_2022), "UTInstant(Year(2022))")
+    @test endswith(repr(second_quarter_of_200), "UTInstant(Quarter(801))")
+    @test endswith(repr(third_week_of_1935), "UTInstant(Week(100966))")
+    @test endswith(repr(hundredth_day_of_year_54620), "UTInstant(Day(19949645))")
+    @test endswith(repr(undated_12), "Undated(12)")
+end
+
+# Efficient (no overhead over Int)
+@testset "space" begin
+    @test Base.summarysize(year_2022) <= sizeof(Int)
+    @test Base.summarysize(second_quarter_of_200) <= sizeof(Int)
+    @test Base.summarysize(third_week_of_1935) <= sizeof(Int)
+    @test Base.summarysize(hundredth_day_of_year_54620) <= sizeof(Int)
+end
+
+@testset "time" begin
+    baseline = @belapsed x + 1 setup = (x=1) samples=10 evals=100
+    @test baseline < 1e-8 # 1+1 should take less than ten nanoseconds on a development machine
+    threshold = 1.2baseline # to accommodate noise
+    @test (@belapsed x + Year(1) setup = (x=$year_2022) samples=10 evals=100) < threshold
+    @test (@belapsed x + Quarter(1) setup = (x=$second_quarter_of_200) samples=10 evals=100) < threshold
+    @test (@belapsed x + Week(1) setup = (x=$third_week_of_1935) samples=10 evals=100) < threshold
+    @test (@belapsed x + Day(1) setup = (x=$hundredth_day_of_year_54620) samples=10 evals=100) < threshold
 end
