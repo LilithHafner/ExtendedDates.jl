d2fa4e753aad61f2fd745d43a81bfc8986dd6162
Documenter.jl <documenter@juliadocs.github.io>
2022-06-17 12:52:12 +0000

    build based on f8a61f9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ExtendedDates","category":"page"},{"location":"#ExtendedDates","page":"Home","title":"ExtendedDates","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ExtendedDates.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ExtendedDates]","category":"page"}]
+}
