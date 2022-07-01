283fad2f0e91a220168496d7592bac188e775ce9
Lilith Hafner <Lilith.Hafner@gmail.com>
2022-07-01 13:08:33 -0400

    add docstrings

--- src/ExtendedDates.jl
+++ src/ExtendedDates.jl
@@ -19,10 +19,50 @@ value(a::UTInstant) = value(a.periods)
 const YearPeriod = Union{Month, Quarter, Semester, Year}
 
 # Defining the epochs
+"""
+    epoch(::Type{Period})
+
+The canonical epoch of a given type of period. For most period types the epoch is 
+Saturday, January 1, year 0. For week periods, it is Monday, January 3, year 0.
+
+```jldoctest
+julia> ExtendedDates.epoch(Quarter)
+0000-01-01
+
+julia> ExtendedDates.epoch(Week)
+0000-01-03
+```
+"""
 epoch(::Type{<:Period}) = Date(0)
 epoch(::Type{Week}) = Date(0, 1, 3) # Monday
 
 # Non overflow checking constructors
+"""
+    period(::Type{<:Period}, year::Integer, subperiod::Integer = 1)
+
+Construct a period from a year, subperiod, and frequency.
+
+These periods are represented as an Int64 number of periods since an epoch defined by the
+`ExtendedDates.epoch` function. For most period types the epoch is Saturday, January 1, 
+year 0. For week periods, it is Monday, January 3, year 0.
+
+```jldoctest
+julia> x = period(Semester, 2022, 1)
+ExtendedDates.UTInstant(Semester(4044))
+
+julia> println(x)
+2022-S1
+
+julia> Date(x)
+2022-01-01
+
+julia> Date(period(Week, 0))
+0000-01-03
+
+julia> Date(period(Day, 0))
+0000-01-01
+```
+"""
 period(P::Type{<:Period}, year::Integer, subperiod::Integer=1) =
     UTInstant(P((Date(year) - epoch(P)) ÷ P(1) + subperiod - 1))
 period(P::Type{<:YearPeriod}, year::Integer, subperiod::Integer=1) =
@@ -33,8 +73,41 @@ Date(p::UTInstant) = Date(0) + p.periods
 Date(p::UTInstant{Week}) = Date(0, 1, 3) + p.periods
 
 # Fallback accessors for frequency, year, subperiod
+"""
+    frequency(::UTInstant{<:Period})
+
+The frequency of a period.
+
+```jldoctest
+julia> frequency(period(Year, 1960))
+1 year
+```
+"""
 frequency(::UTInstant{P}) where P <: Period = oneunit(P)
+"""
+    year(::UTInstant{<:Period})
+
+The year of a period.
+
+```jldoctest
+julia> year(period(Day, 1960, 12))
+1960
+```
+"""
 year(p::UTInstant) = year(Date(p))
+"""
+    year(::UTInstant{<:Period})
+
+The subperiod of a period within a year. Numbering starts at one.
+
+```jldoctest
+julia> subperiod(period(Day, 1960, 12))
+12
+
+julia> Date(period(Day, 1960, 12))
+1960-01-12
+```
+"""
 subperiod(p::UTInstant) = cld((Date(p) - floor(Date(p), Year)), frequency(p)) + 1
 
 # Avoid conversion to Date for Year based periods
